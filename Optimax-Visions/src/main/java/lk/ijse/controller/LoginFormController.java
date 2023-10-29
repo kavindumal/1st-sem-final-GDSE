1de0cddf5648d99b8becf2a90265bcb24657b1e0
kavindumal <kavindu11250403@gmail.com>
2023-10-29 20:07:02 +0530

    loginForm UI finished

--- Optimax-Visions/src/main/java/lk/ijse/controller/LoginFormController.java
+++ Optimax-Visions/src/main/java/lk/ijse/controller/LoginFormController.java
@@ -1,6 +1,45 @@
 package lk.ijse.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class LoginFormController {
 
+    @FXML
+    private JFXButton createNewAccBtn;
+
+    @FXML
+    private JFXButton forgotPwBtn;
+
+    @FXML
+    private JFXButton loginBtn;
+
+    @FXML
+    private AnchorPane loginPane;
+
+    @FXML
+    private PasswordField passwordTxt;
+
+    @FXML
+    private TextField usernameTxt;
+
+    @FXML
+    void createNewAccBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void forgotPwBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void loginBtnOnAction(ActionEvent event) {
+
+    }
 
 }
