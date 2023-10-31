e84f04ea046bba17ea1cb6f09846208c9d3a7011
kavindumal <kavindu11250403@gmail.com>
2023-10-31 16:54:08 +0530

    OtpVerificationModel continue.....

--- Optimax-Visions/src/main/java/lk/ijse/model/OtpVerificationModel.java
+++ Optimax-Visions/src/main/java/lk/ijse/model/OtpVerificationModel.java
@@ -3,7 +3,8 @@ package lk.ijse.model;
 import lk.ijse.dto.OtpVerificationDto;
 
 public class OtpVerificationModel {
+    OtpVerificationDto otpVerificationDto;
     public OtpVerificationModel(OtpVerificationDto otpVerificationDto){
-
+        this.otpVerificationDto = otpVerificationDto;
     }
 }
