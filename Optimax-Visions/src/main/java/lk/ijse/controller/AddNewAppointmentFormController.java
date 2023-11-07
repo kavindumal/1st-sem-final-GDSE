d5b024e60637a18394563b2afb271df2683a64c4
kavindumal <kavindu11250403@gmail.com>
2023-11-07 18:15:27 +0530

    optimax visions version 2.1

--- Optimax-Visions/src/main/java/lk/ijse/controller/AddNewAppointmentFormController.java
+++ Optimax-Visions/src/main/java/lk/ijse/controller/AddNewAppointmentFormController.java
@@ -1,5 +1,14 @@
 package lk.ijse.controller;
 
-public class AddNewAppointmentFormController {
+import javafx.fxml.Initializable;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddNewAppointmentFormController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
