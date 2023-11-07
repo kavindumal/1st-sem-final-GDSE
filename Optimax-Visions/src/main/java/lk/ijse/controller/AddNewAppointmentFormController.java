2f0168f9b9c792b1c115c37ff47ebde316b7b18d
kavindumal <kavindu11250403@gmail.com>
2023-11-07 23:01:42 +0530

    optimax visions version 2.2

--- Optimax-Visions/src/main/java/lk/ijse/controller/AddNewAppointmentFormController.java
+++ Optimax-Visions/src/main/java/lk/ijse/controller/AddNewAppointmentFormController.java
@@ -1,14 +1,46 @@
 package lk.ijse.controller;
 
+import com.jfoenix.controls.*;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import com.jfoenix.controls.JFXDatePicker;
+import javafx.scene.layout.GridPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AddNewAppointmentFormController implements Initializable {
+    @FXML
+    private AnchorPane appoitmentPane;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        GridPane gridPane = new GridPane();
+        int numRows = 5; // Number of rows in the calendar grid
+        int numCols = 7; // Number of columns in the calendar grid
 
+        // Create a 2D array of Buttons to represent the calendar
+        Button[][] dateButtons = new Button[numRows][numCols];
+        gridPane.setHgap(10.0);
+        gridPane.setVgap(10.0);
+
+
+        for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col < numCols; col++) {
+                Button button = new Button();
+                // Customize the button style and action as needed
+                button.setMinSize(60, 60);
+                button.setText("1");
+                button.setStyle("-fx-background-radius: 20;");
+                gridPane.add(button, col, row);
+                dateButtons[row][col] = button;
+            }
+        }
+
+        // Customize the date highlighting (e.g., holidays)
+        dateButtons[2][4].setStyle("-fx-base: #ff0000;"); // Highlighting a specific date
+        appoitmentPane.getChildren().add(gridPane);
     }
 }
--- Optimax-Visions/src/main/java/lk/ijse/controller/HomeFormController.java
+++ Optimax-Visions/src/main/java/lk/ijse/controller/HomeFormController.java
@@ -1,11 +1,28 @@
 package lk.ijse.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class HomeFormController {
 
     @FXML
+    private JFXButton newAppointmentBtn;
+
+    @FXML
     private AnchorPane homePane;
 
+    public void newAppointmentBtnOnAction(ActionEvent actionEvent) {
+        homePane.getChildren().clear();
+        try {
+            homePane.getChildren().add(FXMLLoader.load(Objects.requireNonNull(this.getClass().getResource("/view/addNewAppointmentForm.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
