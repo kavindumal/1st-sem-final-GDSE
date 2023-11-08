1962a4eed01cf31b570cbda3e56138c662302df0
kavindumal <kavindu11250403@gmail.com>
2023-11-08 09:31:56 +0530

    optimax visions version 2.5

--- Optimax-Visions/src/main/java/lk/ijse/controller/AddNewAppointmentFormController.java
+++ Optimax-Visions/src/main/java/lk/ijse/controller/AddNewAppointmentFormController.java
@@ -1,46 +1,19 @@
 package lk.ijse.controller;
 
-import com.jfoenix.controls.*;
+import com.calendarfx.view.page.WeekPage;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
-import com.jfoenix.controls.JFXDatePicker;
-import javafx.scene.layout.GridPane;
-import org.checkerframework.checker.units.qual.C;
 
-import java.awt.*;
-import java.net.URL;
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.TemporalAdjusters;
-import java.util.Calendar;
-import java.util.ResourceBundle;
+public class AddNewAppointmentFormController {
 
-public class AddNewAppointmentFormController implements Initializable {
+    @FXML
+    private Label appointmentIdLbl;
+
     @FXML
     private AnchorPane appoitmentPane;
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    @FXML
+    private WeekPage calanderWeekPage;
 
-    }
-
-    public int dateCount() {
-        LocalDate currentDate = LocalDate.now();
-        LocalDate firstDayOfMonth = currentDate.with(TemporalAdjusters.firstDayOfMonth());
-        DayOfWeek startDay = firstDayOfMonth.getDayOfWeek();
-
-        int count = -1;
-        if (startDay.equals(DayOfWeek.MONDAY)) count = 0;
-        else if (startDay.equals(DayOfWeek.TUESDAY)) count = 1;
-        else if (startDay.equals(DayOfWeek.WEDNESDAY)) count = 2;
-        else if (startDay.equals(DayOfWeek.THURSDAY)) count = 3;
-        else if (startDay.equals(DayOfWeek.FRIDAY)) count = 4;
-        else if (startDay.equals(DayOfWeek.SATURDAY)) count = 5;
-        else if (startDay.equals(DayOfWeek.SUNDAY)) count = 6;
-        System.out.println(count);
-        return count;
-    }
 }
