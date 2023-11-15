b7b4cf6d70359f0c5dade1c8a1c59c0111580270
kavindumal <kavindu11250403@gmail.com>
2023-11-15 19:57:00 +0530

    optimax visions version 6.3

--- Optimax-Visions/src/main/java/lk/ijse/controller/PrescriptionResultFormController.java
+++ Optimax-Visions/src/main/java/lk/ijse/controller/PrescriptionResultFormController.java
@@ -1,5 +1,142 @@
 package lk.ijse.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import org.kordamp.ikonli.javafx.FontIcon;
+
 public class PrescriptionResultFormController {
+
+    @FXML
+    private FontIcon addtoCartFontIcon;
+
+    @FXML
+    private ImageView resultImg1;
+
+    @FXML
+    private ImageView resultImg2;
+
+    @FXML
+    private ImageView resultImg3;
+
+    @FXML
+    private ImageView resultImg4;
+
+    @FXML
+    private ImageView resultImg5;
+
+    @FXML
+    private ImageView resultImg6;
+
+    @FXML
+    private ImageView resultImg7;
+
+    @FXML
+    private ImageView resultImg8;
+
+    @FXML
+    private Pane results1Pane;
+
+    @FXML
+    private Pane results2Pane;
+
+    @FXML
+    private Pane results3Pane;
+
+    @FXML
+    private Pane results4Pane;
+
+    @FXML
+    private Pane results5Pane;
+
+    @FXML
+    private Pane results6Pane;
+
+    @FXML
+    private Pane results7Pane;
+
+    @FXML
+    private Pane results8Pane;
+
+    @FXML
+    void results1PaneOnMouseEntered(MouseEvent event) {
+
+    }
+
+    @FXML
+    void results1PaneOnMouseExited(MouseEvent event) {
+
+    }
+
+    @FXML
+    void results2PaneOnMouseEntered(MouseEvent event) {
+
+    }
+
+    @FXML
+    void results2PaneOnMouseExited(MouseEvent event) {
+
+    }
+
+    @FXML
+    void results3PaneOnMouseEntered(MouseEvent event) {
+
+    }
+
+    @FXML
+    void results3PaneOnMouseExited(MouseEvent event) {
+
+    }
+
+    @FXML
+    void results4PaneOnMouseEntered(MouseEvent event) {
+
+    }
+
+    @FXML
+    void results4PaneOnMouseExited(MouseEvent event) {
+
+    }
+
+    @FXML
+    void results5PaneOnMouseEntered(MouseEvent event) {
+
+    }
+
+    @FXML
+    void results5PaneOnMouseExited(MouseEvent event) {
+
+    }
+
+    @FXML
+    void results6PaneOnMouseEntered(MouseEvent event) {
+
+    }
+
+    @FXML
+    void results6PaneOnMouseExited(MouseEvent event) {
+
+    }
+
+    @FXML
+    void results7PaneOnMouseEntered(MouseEvent event) {
+
+    }
+
+    @FXML
+    void results7PaneOnMouseExited(MouseEvent event) {
+
+    }
+
+    @FXML
+    void results8PaneOnMouseEntered(MouseEvent event) {
+
+    }
+
+    @FXML
+    void results8PaneOnMouseExited(MouseEvent event) {
+
+    }
 
 }
